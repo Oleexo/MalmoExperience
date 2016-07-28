80864dd2539dd0b5e74836b25269eaa040768c1e
Maxime Charles <maxime.charles@outlook.fr>
2016-07-28 18:57:58 +0200

    Add base project

---
+++ MalmoExperience/Framework/Player.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Research.Malmo;
+
+namespace RunMission.Framework {
+	public class Player {
+		private AgentHost _agenthost;
+
+		public Player(AgentHost agenthost) {
+			_agenthost = agenthost;
+		}
+	}
+}
